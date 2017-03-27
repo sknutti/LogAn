7b2046ba6d9d514db6acd926476bcb1a779b194b
Scott Knutti <sknutti@gmail.com>
2017-03-27 16:18:33 -0600

    removed files that should be ignored

--- src/test/kotlin/com/stgconsulting/aut/LogAnalyzerTest3.kt
+++ src/test/kotlin/com/stgconsulting/aut/LogAnalyzerTest3.kt
@@ -1,7 +1,6 @@
 package com.stgconsulting.aut
 
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
